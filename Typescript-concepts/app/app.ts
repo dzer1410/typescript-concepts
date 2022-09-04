8ae9937e80b3320dbf066aeb3229cfce5d842f56
Daniel Zermeño <dzermeno@nic.mx>
2022-09-04 13:52:03 -0500

    practice of typescrit types, built-in types, union types and assertion types

--- Typescript-concepts/app/app.ts
+++ Typescript-concepts/app/app.ts
@@ -1,7 +1,15 @@
 function startGame(){
+
+    let playerName: string = 'Audrey';
+    logPlayer(playerName);
+
     var messageElement = document.getElementById('messages');
     messageElement!.innerText = 'Welcome to Multitash! Starting new game...';
 
 }
 
-document.getElementById('startGame')!.addEventListener('click', startGame);
+function logPlayer(name: string){
+    console.log(`New game starting for player: ${name}`);
+}
+
+document.getElementById('startGame')!.addEventListener('click', startGame); 
